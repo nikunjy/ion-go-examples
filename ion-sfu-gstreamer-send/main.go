af5c09b24ca190e0b4843ce88eb9b3653ba60810
Nikunj Yadav <nikunj@brex.com>
2021-09-13 22:01:05 -0700

    Nikunj exp

--- ion-sfu-gstreamer-send/main.go
+++ ion-sfu-gstreamer-send/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"context"
 	"flag"
 	"fmt"
 
@@ -15,6 +16,90 @@ var (
 	log = ilog.NewLoggerWithFields(ilog.DebugLevel, "", nil)
 )
 
+func Feed(ctx context.Context, addr string, roomName string) error {
+	session := roomName
+	codec := "h264"
+	videoSrc := "autovideosrc !  videoconvert ! videoscale"
+	audioSrc := "autoaudiosrc"
+	fmt.Println(videoSrc, audioSrc)
+	mimeType := fmt.Sprintf("video/%s", codec)
+	// add stun servers
+	webrtcCfg := webrtc.Configuration{
+		ICEServers: []webrtc.ICEServer{
+			webrtc.ICEServer{
+				URLs: []string{"stun:stun.l.google.com:19302"},
+			},
+		},
+	}
+
+	config := sdk.Config{
+		WebRTC: sdk.WebRTCTransportConfig{
+			Configuration: webrtcCfg,
+			VideoMime:     mimeType,
+		},
+	}
+
+	// new sdk engine
+	e := sdk.NewEngine(config)
+	// get a client from engine
+	c, err := sdk.NewClient(e, addr, "gstreamer")
+	if err != nil {
+		return err
+	}
+	var peerConnection *webrtc.PeerConnection = c.GetPubTransport().GetPeerConnection()
+
+	peerConnection.OnICEConnectionStateChange(func(state webrtc.ICEConnectionState) {
+		log.Infof("Connection state changed: %s", state)
+	})
+
+	if err != nil {
+		log.Errorf("client err=%v", err)
+		return err
+	}
+
+	err = e.AddClient(c)
+	if err != nil {
+		return err
+	}
+
+	videoTrack, err := webrtc.NewTrackLocalStaticSample(webrtc.RTPCodecCapability{MimeType: mimeType, ClockRate: 90000, Channels: 0, SDPFmtpLine: "packetization-mode=1;profile-level-id=42e01f", RTCPFeedback: nil}, "video11", "pion2")
+	if err != nil {
+		return err
+	}
+
+	_, err = peerConnection.AddTrack(videoTrack)
+	if err != nil {
+		return err
+	}
+
+	audioTrack, err := webrtc.NewTrackLocalStaticSample(webrtc.RTPCodecCapability{MimeType: "audio/opus"}, "audio11", "pion2")
+	if err != nil {
+		return err
+	}
+	_, err = peerConnection.AddTrack(audioTrack)
+	if err != nil {
+		return err
+	}
+
+	// client join a session
+	err = c.Join(session, sdk.NewJoinConfig().SetNoSubscribe())
+
+	if err != nil {
+		log.Errorf("join err=%v", err)
+		return err
+	}
+
+	// Start pushing buffers on these tracks
+	gst.CreatePipeline("opus", []*webrtc.TrackLocalStaticSample{audioTrack}, audioSrc).Start()
+	gst.CreatePipeline(codec, []*webrtc.TrackLocalStaticSample{videoTrack}, videoSrc).Start()
+
+	select {
+	case <-ctx.Done():
+		return ctx.Err()
+	}
+	return nil
+}
+
 func main() {
 	// parse flag
 	var session, addr string
@@ -24,6 +109,10 @@ func main() {
 	flag.StringVar(&session, "session", "test room", "join session name")
 	flag.StringVar(&codec, "codec", "vp8", "codec name")
 
+	ctx := context.Background()
+	if err := Feed(ctx, addr, session); err != nil {
+		panic(err)
+	}
 	audioSrc := flag.String("audio-src", "audiotestsrc", "GStreamer audio src")
 	videoSrc := flag.String("video-src", "videotestsrc", "GStreamer video src")
 	flag.Parse()
@@ -45,7 +134,7 @@ func main() {
 	config := sdk.Config{
 		WebRTC: sdk.WebRTCTransportConfig{
 			Configuration: webrtcCfg,
-			VideoMime: mimeType,
+			VideoMime:     mimeType,
 		},
 	}
 
@@ -103,4 +192,3 @@ func main() {
 
 	select {}
 }
-
